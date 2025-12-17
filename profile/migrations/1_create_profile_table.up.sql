caa3996da13add72ab68710642f9ddf10b947795
tatapomangol <deadpolmail.ru@gmail.com>
2025-12-18 02:02:29 +0500

    add index

--- profile/migrations/1_create_profile_table.up.sql
+++ profile/migrations/1_create_profile_table.up.sql
@@ -1,1 +1,7 @@
-DROP TABLE IF EXISTS profile;
+CREATE TABLE profile (
+    subject_id TEXT PRIMARY KEY, 
+    alias TEXT NOT NULL,
+    avatar_url TEXT,
+    version INT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+)
