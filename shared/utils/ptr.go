f2d60a491afeaf583cc78ac63eb79e9754e35275
tatapomangol <deadpolmail.ru@gmail.com>
2026-01-04 05:44:05 +0500

    add equal ptr func

--- shared/utils/ptr.go
+++ shared/utils/ptr.go
@@ -3,3 +3,13 @@ package utils
 func StringPtr(s string) *string {
 	return &s
 }
+
+func StringPtrEqual(a, b *string) bool {
+	if a == nil && b == nil {
+		return true
+	}
+	if a == nil || b == nil {
+		return false
+	}
+	return *a == *b
+}
